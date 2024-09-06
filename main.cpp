0cbe410015383e888474cd1f4be3a64b54ee0086
Bodey Lassiter <blassit1@ut.utm.edu>
2024-09-06 14:40:51 -0500

    Finding Sums of numbers 1 to n.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
-}
+  int sum(int n) {
+    int total = 0;
+    for (int i = 1; i <=n; ++i){
+      total += i;
+    }
+    return total;
+  }
+
+  int main() {
+    int n;
+
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+
+    int result = sum(n);
+    std::cout << "The sum of number 1 to " << n << " is: " << result << std::endl;
+
+    return 0;
+
+  }
