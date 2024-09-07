938972d592a9ea69cef1040df67c41023750b15a
Bodey Lassiter <blassit1@ut.utm.edu>
2024-09-06 22:09:53 -0500

    Adding Feature Branch / Product

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@
     }
     return total;
   }
+int product(int n) {
+  int total = 1;
+  for (int i = 1; i <= n; ++i){
+    total *= i;
+  }
+  return total;
+}
 
   int main() {
     int n;
@@ -16,7 +23,11 @@
 
     int result = sum(n);
     std::cout << "The sum of number 1 to " << n << " is: " << result << std::endl;
-
+    result = 0;
+    result = product(n);
+    std::cout << "The product of number 1 to " << n << " is: " << result << std::endl;
+    
+    
     return 0;
 
   }
